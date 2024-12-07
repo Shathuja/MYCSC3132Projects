a580fa7cf1bcceeaccd98f2d0f5358e99496c4e4
Shathuja <139630207+Shathuja@users.noreply.github.com>
2024-12-07 06:33:49 +0000

    add day2 folder

---
+++ Day2/gettable.php
@@ -0,0 +1,43 @@
+<?php
+require_once 'dbconf.php';
+
+function PrintTable($tableName,$connect)
+{
+
+try {
+
+	$sql = "SELECT * FROM $tableName";
+
+	$result = mysqli_query($connect,$sql);
+	if (mysqli_num_rows($result)>0) {
+		echo "<table border='1'>";
+		$col = mysqli_fetch_fields($result);
+        $columns=mysqli_num_fields($result);
+        echo "<tr>";
+        echo "<th colspan='$columns'>$tableName</th>";
+        echo "</tr>";
+		echo "<tr>";
+		foreach ($col as $value) {
+			echo "<th>$value->name</th>";
+		}
+		echo "</tr>";
+		
+		while ($row = mysqli_fetch_assoc($result)) {
+			echo "<tr>";
+			foreach ($row as $key => $value) {
+				echo "<td>$value</td>";
+			}
+			echo "</tr>";
+		}
+		echo "</table>"."<br>";
+	} else {
+		echo "No results";
+	}
+	
+} catch (Exception $e) {
+	die($e->getMessage());
+}
+}
+PrintTable("Student",$connect);
+PrintTable("Book",$connect);
+?>
