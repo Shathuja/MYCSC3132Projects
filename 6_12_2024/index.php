a9ab8fc9e4a4e98fd9faf68124be5ab4d96b5a15
Shathuja <139630207+Shathuja@users.noreply.github.com>
2024-12-07 15:38:58 +0000

    add gettable

---
+++ 6_12_2024/index.php
@@ -0,0 +1,19 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Pratical 2020ASP50</h1><br><br>
+
+    <a href="getdata.php">Gettable</a><br>
+    
+    <a href="print.php">Print</a><br>
+
+
+    
+</body>
+</html>
