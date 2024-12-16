9bf196c5c213a00750a94cbb245792ff1b10e507
Shathuja <139630207+Shathuja@users.noreply.github.com>
2024-12-16 11:23:19 +0000

    create

--- 13_12_2024/dbconf.php
+++ 13_12_2024/dbconf.php
@@ -1,23 +1,19 @@
 <?php
-
-define('SERVERNAME', '127.0.0.1:3306');
+define('HOST', '127.0.0.1:3306');
 define('USERNAME', 'root');
-define('PASSWORD', 'mariadb');
-define('DBNAME', 'Studentdb');
+define('PWD', 'mariadb');
+define('DB', 'web');
 
-try{
-    $connect = mysqli_connect(SERVERNAME,USERNAME,PASSWORD,DBNAME);
-
-    if (!$connect){
-        die("connection failed".mysqli_connect_error());
-    } else {
-        echo "Connected successfully";
+try {
+    $connection = mysqli_connect(HOST,USERNAME,PWD,DB);
+    if(!$connection){
+       die("Database not connected!");
     }
-}
-catch(Exception $e) {
+    else{
+        //echo "Database connected succefully!";
+    }
+} catch (Exception $e) {
     die($e->getMessage());
 }
-
-
-
+echo "<br>";
 ?>
